908df8775bee0a6b2d9caf5c1eb8f9cbd1398f28
Clifford Curry <cliff@Cliffs-MacBook-Pro.local>
2014-07-02 19:32:08 -0700

    Initial Commit

---
+++ week3-homework/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  week3-homework
+//
+//  Created by Clifford Curry on 7/2/14.
+//  Copyright (c) 2014 Google.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ week3-homeworkTests/week3_homeworkTests.m
@@ -0,0 +1,34 @@
+//
+//  week3_homeworkTests.m
+//  week3-homeworkTests
+//
+//  Created by Clifford Curry on 7/2/14.
+//  Copyright (c) 2014 Google.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface week3_homeworkTests : XCTestCase
+
+@end
+
+@implementation week3_homeworkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
